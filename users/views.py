4fc6daf3c6667f9c05bd0dad6086d19cfe981f4f
louannl <louann_loizou@hotmail.com>
2021-10-21 15:33:49 +0100

    Authentication - User Registration

--- users/views.py
+++ users/views.py
@@ -4,9 +4,13 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 
 def loginUser(request):
+    page = 'login'
+    context = {'page': page}
+
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -27,13 +31,38 @@ def loginUser(request):
         else:
             messages.error(request, 'Incorrect Username or password')
 
-    return render(request, 'users/login_register.html')
+    return render(request, 'users/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     messages.error(request, 'User was logged out!')
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            # commit false gives us an instance
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User account was created!')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.error(
+                request, 'An error has occurred during registration.')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
