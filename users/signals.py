f8f03a8612b80d5cb23e5a738d64a3a274dce75d
louannl <louann_loizou@hotmail.com>
2021-11-05 17:33:08 +0000

    User Actions: User Edit Profile

--- users/signals.py
+++ users/signals.py
@@ -19,6 +19,18 @@ def createProfile(sender, instance, created, **kwargs):
         )
 
 
+def updateUser(sender, instance, created, **kwards):
+    profile = instance
+    user = profile.user
+    # if we didn't add created == False, it would trigger createProfile and create
+    # an infinite loop
+    if created == False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
 def deleteUser(sender, instance, **kwargs):
     # get profile's user (instance is the profile)
     user = instance.user
@@ -29,5 +41,5 @@ def deleteUser(sender, instance, **kwargs):
 
 # signals
 post_save.connect(createProfile, sender=User)
-
+post_save.connect(updateUser, sender=Profile)
 post_delete.connect(deleteUser, sender=Profile)
