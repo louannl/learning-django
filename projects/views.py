19fc899c69f21063d743d9bf385079d311f35a2a
louannl <louann_loizou@hotmail.com>
2021-11-08 14:28:59 +0000

    User Actions: Add, Edit, Delete Skills

--- projects/views.py
+++ projects/views.py
@@ -28,7 +28,7 @@ def createProject(request):
             project = form.save(commit=False)
             project.owner = profile
             project.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
@@ -45,7 +45,7 @@ def updateProject(request, pk):
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('projects')
+            return redirect('account')
 
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
@@ -58,7 +58,7 @@ def deleteProject(request, pk):
 
     if request.method == 'POST':
         project.delete()
-        return redirect('projects')
+        return redirect('account')
 
     context = {'object': project}
-    return render(request, "projects/delete_template.html", context)
+    return render(request, "delete_template.html", context)
